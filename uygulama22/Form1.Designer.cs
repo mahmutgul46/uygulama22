212c0b4665c0e797b7b102e5db463b08590a57d2
mahmut559 <mahmut559@RPC03>
2024-03-21 12:11:43 +0300

    Proje dosyası ekle.

---
+++ uygulama22/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace uygulama22
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKitapEkle = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.listeVeri = new System.Windows.Forms.ListBox();
+            this.txtKitapAdi = new System.Windows.Forms.TextBox();
+            this.txtKitapYazari = new System.Windows.Forms.TextBox();
+            this.txtKitapTuru = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKitapEkle
+            // 
+            this.btnKitapEkle.Location = new System.Drawing.Point(123, 145);
+            this.btnKitapEkle.Name = "btnKitapEkle";
+            this.btnKitapEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnKitapEkle.TabIndex = 0;
+            this.btnKitapEkle.Text = "Kitap Ekle";
+            this.btnKitapEkle.UseVisualStyleBackColor = true;
+            this.btnKitapEkle.Click += new System.EventHandler(this.btnKitapEkle_Click);
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(289, 145);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(75, 23);
+            this.btnListele.TabIndex = 1;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // listeVeri
+            // 
+            this.listeVeri.FormattingEnabled = true;
+            this.listeVeri.Location = new System.Drawing.Point(270, 12);
+            this.listeVeri.Name = "listeVeri";
+            this.listeVeri.Size = new System.Drawing.Size(115, 121);
+            this.listeVeri.TabIndex = 2;
+            // 
+            // txtKitapAdi
+            // 
+            this.txtKitapAdi.Location = new System.Drawing.Point(113, 25);
+            this.txtKitapAdi.Name = "txtKitapAdi";
+            this.txtKitapAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtKitapAdi.TabIndex = 3;
+            // 
+            // txtKitapYazari
+            // 
+            this.txtKitapYazari.Location = new System.Drawing.Point(113, 63);
+            this.txtKitapYazari.Name = "txtKitapYazari";
+            this.txtKitapYazari.Size = new System.Drawing.Size(100, 20);
+            this.txtKitapYazari.TabIndex = 4;
+            // 
+            // txtKitapTuru
+            // 
+            this.txtKitapTuru.Location = new System.Drawing.Point(113, 102);
+            this.txtKitapTuru.Name = "txtKitapTuru";
+            this.txtKitapTuru.Size = new System.Drawing.Size(100, 20);
+            this.txtKitapTuru.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Kitap Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Kitap Yazarı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Kitap Türü";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKitapTuru);
+            this.Controls.Add(this.txtKitapYazari);
+            this.Controls.Add(this.txtKitapAdi);
+            this.Controls.Add(this.listeVeri);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.btnKitapEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKitapEkle;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.ListBox listeVeri;
+        private System.Windows.Forms.TextBox txtKitapAdi;
+        private System.Windows.Forms.TextBox txtKitapYazari;
+        private System.Windows.Forms.TextBox txtKitapTuru;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ uygulama22/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama22
+{
+    public partial class Form1 : Form
+    {
+        ArrayList kaynakVeri = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKitapEkle_Click(object sender, EventArgs e)
+        {
+            Kitaplar kitap = new Kitaplar();
+            kitap.KitapAdi = txtKitapAdi.Text;
+            kitap.KitapYazari = txtKitapYazari.Text;
+            kitap.KitapTuru = txtKitapTuru.Text;
+            kaynakVeri.Add(kitap.KitapAdi);
+            kaynakVeri.Add(kitap.KitapYazari);
+            kaynakVeri.Add(kitap.KitapTuru);
+
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            listeVeri.DataSource = null;
+            listeVeri.DataSource = kaynakVeri;
+            listeVeri.DisplayMember = "KitapAdi";
+            listeVeri.DisplayMember ="KitapYazari";
+            listeVeri.DisplayMember = "KitapTuru";
+        }
+    }
+}
